cfc0969b2d1bce6fed393930d7e01e3369eb19c2
JayaNirenjan45 <126094436+JayaNirenjan45@users.noreply.github.com>
2025-01-31 23:32:36 +0530

    Time: 47 ms (5.01%), Space: 104.8 MB (6.2%) - LeetHub

---
+++ 0932-monotonic-array/0932-monotonic-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        vector<int> n = nums;
+        sort(n.begin(),n.end());
+        if(n == nums)
+         return true;
+        reverse(n.begin(),n.end());
+        if(n == nums)
+         return true;
+        return false;
+    }
+};
